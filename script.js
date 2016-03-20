3a9583465e4b19ceebeca013c3a5db4e220ed903
goodtalks <tjwls0404@gmail.com>
2016-03-19 21:00:35 -0400

    Added files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+ $(document).ready(function(){
+    var navbarHeight = 64;
+    var $imageContainer = $('.image-container');
+
+    function fillHeaderImage(nav, $img){
+      var imgHeight = $(window).height() - nav;
+      $img.css('height', imgHeight+'px');
+    }
+    fillHeaderImage(navbarHeight, $imageContainer);
+    $(window).resize(fillHeaderImage(navbarHeight, $imageContainer));
+
+    $('.scrollspy').scrollSpy();
+    $(".button-collapse").sideNav();
+    $('.parallax').parallax();
+  });
