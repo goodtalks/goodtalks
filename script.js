5c85f463131fbd2b44f9db2f5acc74d3a717b198
goodtalks <tjwls0404@gmail.com>
2016-03-22 01:48:54 -0400

    Added files via upload

--- script.js
+++ script.js
@@ -2,13 +2,12 @@
     $(".button-collapse").sideNav();
 
     $('.parallax').parallax();
-    $('.scrollspy').scrollSpy();
 
      $(".nav-menu a").click(function (e){
         e.preventDefault();
         var link = $(this).attr('href');
         $('html, body').animate({
-            scrollTop: $(link).offset().top - 180
+            scrollTop: $(link).offset().top - 64
         }, 300);
     });
   });
