d51d349c3a7e87be082c0910f699e76575c26b76
goodtalks <tjwls0404@gmail.com>
2016-03-27 17:42:03 -0400

    Added files via upload

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
- $(document).ready(function(){
-    $(".button-collapse").sideNav();
+$(document).ready(function(){
+  $(".button-collapse").sideNav();
 
-    $('.parallax').parallax();
+  $('.parallax').parallax();
+  $('.modal-trigger').leanModal();
 
-     $(".nav-menu a").click(function (e){
-        e.preventDefault();
-        var link = $(this).attr('href');
-        $('html, body').animate({
-            scrollTop: $(link).offset().top - 64
-        }, 300);
-    });
+  $(".nav-menu a").click(function (e){
+    e.preventDefault();
+    var link = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(link).offset().top - 64
+    }, 300);
   });
+});
